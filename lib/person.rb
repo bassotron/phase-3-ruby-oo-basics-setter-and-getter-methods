118cf189833cf21a3b6ecdd7c18d57f4a4c6dce4
Will Lovell <willard.lovell@icloud.com>
2022-08-31 17:43:17 -0600

    commit tree 050b42bc63de4777636ccd751ffd7ee2ba3fce9f

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+def name=(name)
+	@name = name
+end
+def name
+	@name
+end
+def job=(job)
+	@job = job
+end
+def job 
+	@job
+end
+doug = Person.new
+doug.name = "Doug"
+doug.job = "Programmer"
+
 
 end
